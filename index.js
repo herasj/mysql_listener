7d5315116f9c653c01b4d581cd611609bff7db08
herasj <juanp231100@gmail.com>
2020-03-21 21:05:29 -0500

    Basic Listener

--- index.js
+++ index.js
@@ -8,19 +8,15 @@ const config = {
 };
 
 let zongji = new ZongJi(config);
- 
+console.log("Starting app");
+
+ // Binlog must be started, optionally pass in filters
+zongji.start({
+    includeEvents: ['tablemap', 'writerows', 'updaterows', 'deleterows']
+  });
+
 // Each change to the replication log results in an event
 zongji.on('binlog', function(evt) {
   evt.dump();
 });
  
-// Binlog must be started, optionally pass in filters
-zongji.start({
-  includeEvents: ['tablemap', 'writerows', 'updaterows', 'deleterows']
-});
-
-process.on('SIGINT', function() {
-    console.log('Got SIGINT.');
-    zongji.stop();
-    process.exit();
-  });
